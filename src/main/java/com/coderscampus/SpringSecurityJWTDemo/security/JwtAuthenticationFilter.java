d8c9cc5d75ecb893f8c4dd985ecdd8a72809d91c
fcynnek <kchengfung@gmail.com>
2023-11-13 19:19:42 -0500

    Reverting changes

--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/security/JwtAuthenticationFilter.java
@@ -34,7 +34,6 @@ import com.coderscampus.SpringSecurityJWTDemo.service.UserServiceImpl;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -59,54 +58,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        
-        Cookie accessTokenCookie = null;
-        Cookie refreshTokenCookie = null;
-        
-        if (request.getCookies() != null) {
-        	for (Cookie cookie : request.getCookies()) {
-        		if (cookie.getName().equals("accessToken")) {
-        			accessTokenCookie = cookie;
-        		} else if (cookie.getName().equals("refreshToken")) {
-        			refreshTokenCookie = cookie;
-        		}
-        	}
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
         }
-        
-        int loginAttempt = 0;
-//        String token = accessTokenCookie.getValue();
-        String token = accessTokenCookie != null ? accessTokenCookie.getValue() : null;
-        
-        while (loginAttempt <= 5) {
-        	
-        	if (StringUtils.hasText(token)) {
-//        	if (accessTokenCookie != null) {
-        		try {
-        			String subject = jwtService.extractUserName(token);
-        			
-        			
-        			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        			if (StringUtils.hasText(subject) && authentication == null) {
-        				UserDetails userDetails = userService.userDetailsService().loadUserByUsername(subject);
-        				
-        				if (jwtService.isTokenValid(token, userDetails)) {
-        					SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-        					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken (userDetails,
-        							userDetails.getPassword(),
-        							userDetails.getAuthorities());
-        					securityContext.setAuthentication(authToken);
-        					SecurityContextHolder.setContext(securityContext);
-        					
-        					// if successful login occurs:
-        					break;
-        				}
-        			}
-        		} catch (ExpiredJwtException e) {
-        			token = refreshTokenService.createNewAccessToken(new RefreshTokenRequest(refreshTokenCookie.getValue()));
-        			e.printStackTrace();
-        		}
-        		loginAttempt++;
-        	}
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+        if (StringUtils.isNotEmpty(userEmail)
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService()
+                    .loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                context.setAuthentication(authToken);
+                SecurityContextHolder.setContext(context);
+            }
         }
         filterChain.doFilter(request, response);
         
--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/security/SecurityConfig.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/security/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.coderscampus.SpringSecurityJWTDemo.security;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.coderscampus.SpringSecurityJWTDemo.domain.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,37 +11,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.coderscampus.SpringSecurityJWTDemo.domain.RefreshToken;
-import com.coderscampus.SpringSecurityJWTDemo.domain.Role;
-import com.coderscampus.SpringSecurityJWTDemo.domain.User;
-import com.coderscampus.SpringSecurityJWTDemo.service.RefreshTokenService;
 import com.coderscampus.SpringSecurityJWTDemo.service.UserService;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserService userService;
-    
-    @Autowired
-    private JwtServiceImpl jwtService;
-    
-    @Autowired
-    private RefreshTokenService refreshTokenService;
     
     public SecurityConfig (JwtAuthenticationFilter jwtAuthenticationFilter, UserService userService) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
@@ -70,27 +52,8 @@ public class SecurityConfig {
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(login -> {
 		        	login.loginPage("/signin");
-//		        	login.successForwardUrl("/success");
-//		        	login.failureUrl("/failure"); // this can be linked to a failure message on the failure template
-//		        	login.failureForwardUrl("/error");
-		        	login.successHandler(new AuthenticationSuccessHandler() {
-						
-						@Override
-						public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-								Authentication authentication) throws IOException, ServletException {
-							
-							User user = (User) authentication.getPrincipal();
-					    	String accessToken = jwtService.generateToken(user);
-					    	RefreshToken refreshToken = refreshTokenService.createRefreshToken(user.getId());
-							
-					    	Cookie accessCookie = new Cookie("accessToken", accessToken);
-					    	Cookie refreshCookie = new Cookie("refreshToken", refreshToken.getToken());
-					    	
-					    	response.addCookie(accessCookie);
-					    	response.addCookie(refreshCookie);
-					    	response.sendRedirect("/products");
-						}
-					});
+		        	login.successForwardUrl("/success");
+		        	login.failureForwardUrl("/error");
 		        	login.permitAll();
 		        });
         return http.build();
